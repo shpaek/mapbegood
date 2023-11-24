49336e16e657f4ad6803a6185ac3b7db7f32d5c8
tofubeat <0717jis@daum.net>
2023-11-24 17:20:01 +0900

    [Feat] ourmap의 group관련 repository 생성
    GroupsRepository, GroupThememapRepository, WaitingRepository 생성함

---
+++ src/main/java/com/kosa/mapbegood/domain/ourmap/groups/repository/GroupsRepository.java
@@ -0,0 +1,9 @@
+package com.kosa.mapbegood.domain.ourmap.groups.repository;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+
+import com.kosa.mapbegood.domain.ourmap.groups.entity.Groups;
+
+public interface GroupsRepository extends JpaRepository<Groups, Long>{
+
+}
