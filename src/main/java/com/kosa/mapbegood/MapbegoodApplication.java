e625651a06e31a94a3ddd3ccc15c0630d85a944c
dozy <78740368+yeori316@users.noreply.github.com>
2023-11-24 19:36:33 +0900

    deploy test

--- src/main/java/com/kosa/mapbegood/MapbegoodApplication.java
+++ src/main/java/com/kosa/mapbegood/MapbegoodApplication.java
@@ -6,11 +6,11 @@ import org.springframework.data.jpa.repository.config.EnableJpaAuditing;
 
 @EnableJpaAuditing
 @SpringBootApplication(
-		exclude = {
-				org.springframework.cloud.aws.autoconfigure.context.ContextInstanceDataAutoConfiguration.class,
-				org.springframework.cloud.aws.autoconfigure.context.ContextStackAutoConfiguration.class,
-				org.springframework.cloud.aws.autoconfigure.context.ContextRegionProviderAutoConfiguration.class
-		}
+//		exclude = {
+//				org.springframework.cloud.aws.autoconfigure.context.ContextInstanceDataAutoConfiguration.class,
+//				org.springframework.cloud.aws.autoconfigure.context.ContextStackAutoConfiguration.class,
+//				org.springframework.cloud.aws.autoconfigure.context.ContextRegionProviderAutoConfiguration.class
+//		}
 )
 public class MapbegoodApplication {
  
