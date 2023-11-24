ef7133e1c826a413f469de665b3ae7b7de7dd08c
dozy <78740368+yeori316@users.noreply.github.com>
2023-11-24 17:24:42 +0900

    deploy homework

--- src/main/java/com/kosa/mapbegood/domain/mymap/myplaceFeed/entity/MyplaceFeed.java
+++ src/main/java/com/kosa/mapbegood/domain/mymap/myplaceFeed/entity/MyplaceFeed.java
@@ -1,37 +1,19 @@
 package com.kosa.mapbegood.domain.mymap.myplaceFeed.entity;
 
-import javax.persistence.EmbeddedId;
-import javax.persistence.Entity;
-import javax.persistence.JoinColumn;
-import javax.persistence.MapsId;
-import javax.persistence.OneToOne;
-import javax.persistence.Table;
+//@Data
+//@EqualsAndHashCode(callSuper=false)
+//@Entity
+//@Table
+public class MyplaceFeed {//extends AuditEntity {
+	
+//	@MapsId
+//	@OneToOne
+//	@JoinColumn(name = "myplaceId")
+//	private Myplace myplaceId;
+//	
+//	@OneToOne
+//	@JoinColumn(name = "nickname")
+//	private Member memberNickname;
 
-import com.kosa.mapbegood.domain.common.entity.AuditEntity;
-import com.kosa.mapbegood.domain.member.entity.Member;
-import com.kosa.mapbegood.domain.mymap.myplace.entity.Myplace;
-
-import lombok.Data;
-import lombok.EqualsAndHashCode;
-
-@Data
-@EqualsAndHashCode(callSuper=false)
-@Entity
-@Table
-public class MyplaceFeed extends AuditEntity {
-	
-	@EmbeddedId
-	private MyplaceFeedEmbedded id = new MyplaceFeedEmbedded();
-	
-	@OneToOne
-	@JoinColumn(name = "myplaceId")
-	@MapsId("myplaceId")
-	private Myplace myplaceId;
-	
-	@OneToOne
-	@JoinColumn(name = "nickname")
-	@MapsId("nickname")
-	private Member memberNickname;
-	
 	private String content;
 }
