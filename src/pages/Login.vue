72ce09ef8a2e668446d52a8df1cac515575e6779
dozy <78740368+yeori316@users.noreply.github.com>
2023-11-27 22:40:32 +0900

    feat: login auth

--- src/pages/Login.vue
+++ src/pages/Login.vue
@@ -1,87 +1,14 @@
 <template>
   <div>Login입니다</div>
-  <!-- <form @submit.prevent="loginFormSubmitHandler">
-    <label for="i">아이디:</label
-    ><input type="text" name="email" id="i" v-model="email" /><br />
-    <label for="p">비밀번호:</label
-    ><input type="password" name="pwd" id="p" v-model="pwd" /><br />
-    <input type="checkbox" v-model="checked" />아이디저장
-    <button>로그인</button>
-  </form> -->
-
-  <div class="loginContainer">
-    <span class="logo_mapbegood">MapBeGood</span>
-
-    <article id="mainContent" class="content-article">
-      <div class="cont_login">
-        <div class="login_kakaomail">
-          <form @submit.prevent="loginFormSubmitHandler">
-            <div class="item_form fst">
-              <div class="box_tf">
-                <label class="screen_out" for="loginId--1">계정정보 입력</label
-                ><input
-                  type="text"
-                  id="loginId"
-                  name="loginId"
-                  placeholder=" 이메일 "
-                  class="tf_g"
-                  v-model="email"
-                />
-                <div class="util_tf"></div>
-              </div>
-            </div>
-            <div class="item_form">
-              <div class="box_tf">
-                <label class="screen_out" for="password--2">비밀번호 입력</label
-                ><input
-                  type="password"
-                  id="password--2"
-                  name="password"
-                  placeholder=" 비밀번호 "
-                  autocomplete="off"
-                  class="tf_g"
-                  v-model="pwd"
-                />
-                <div class="util_tf"></div>
-              </div>
-            </div>
-            <div class="set_login">
-              <div class="item_choice">
-                <input
-                  type="checkbox"
-                  id="saveSignedIn--4"
-                  class="inp_choice"
-                  name="saveSignedIn"
-                  v-model="checked"
-                /><label
-                  class="lab_choice"
-                  for="saveSignedIn--4"
-                  id="label-saveSignedIn"
-                  ><span class="ico_comm ico_check"></span>간편로그인 정보
-                  저장</label
-                >
-                <span>
-                  <a
-                    href="/weblogin/find_password?lang=ko&amp;continue=%2Flogin%3Fcontinue%3Dhttps%253A%252F%252Fmap.kakao.com"
-                    class="link_user"
-                    >비밀번호 찾기
-                  </a>
-                </span>
-              </div>
-            </div>
-            <div class="confirm_btn">
-              <button type="submit" class="btn_g highlight submit">
-                로그인
-              </button>
-              <span class="line_or"><span class="txt_or">또는</span></span>
-              <button type="button" class="btn_g">
-                카카오 계정으로 회원가입
-              </button>
-            </div>
-          </form>
-        </div>
-      </div>
-    </article>
+  <div class="login-container">
+    <div class="login-from">
+      <form @submit.prevent="loginFormSubmitHandler">
+        <input type="text" v-model="email" /><br />
+        <input type="password" v-model="password" /><br />
+        <input type="checkbox" v-model="checked" />아이디저장
+        <button>로그인</button>
+      </form>
+    </div>
   </div>
 </template>
 
@@ -89,9 +16,9 @@
 import axios from "axios";
 
 let model = {
-  email: "test@mail.com",
-  pwd: "test",
-  checked: true,
+  email: "",
+  password: "",
+  checked: "",
 };
 
 export default {
@@ -101,7 +28,6 @@ export default {
   },
   methods: {
     loginFormSubmitHandler() {
-      console.log("loginFormSubmitHandler() 호출");
       if (this.checked) {
         localStorage.setItem("mapbegoodId", this.email);
       } else {
@@ -111,7 +37,7 @@ export default {
       const url = `${this.backURL}/auth`;
       let data = {
         email: `${this.email}`,
-        password: `${this.pwd}`,
+        password: `${this.password}`,
       };
 
       axios
@@ -119,108 +45,29 @@ export default {
           withCredentials: true,
         })
         .then((response) => {
-          console.log(response);
-          console.log(response.headers);
-          if (response.data.status == 0) {
-            alert(response.data.msg);
-          } else if (response.data.status == 1) {
-            localStorage.setItem("mapbegoodId", this.email);
-            location.href = "/";
-          }
+          if (response.status == 200) {
+            console.log(response);
 
+            alert("로그인 성공");
+            // location.href = "/";
+          }
           // // API 요청하는 콜마다 헤더에 accessToken 담아 보내도록 설정
           // axios.defaults.headers.common['Authorization'] = `Bearer ${accessToken}`;
         })
         .catch((error) => {
           console.log(error);
-          alert(error.message);
+          alert("로그인 실패");
         });
     },
-    created() {
-      const savedId = localStorage.getItem("mapbegoodId");
-
-      if (savedId != null) {
-        this.email = savedId;
-      }
-    },
+  },
+  created() {
+    const savedId = localStorage.getItem("mapbegoodId");
+    if (savedId != null) {
+      this.email = savedId;
+      this.checked = true;
+    }
   },
 };
 </script>
 
-<style scoped>
-.logo_mapbegood {
-  display: block;
-  width: 88px;
-  height: 27px;
-  margin: 0 auto;
-  background: #fff url(/public/images/logo.png) no-repeat 0 0;
-  background-size: 100px 80px;
-  font-size: 19px;
-  line-height: 27px;
-  text-align: center;
-}
-.content-article {
-  width: 580px;
-  height: 100%;
-  margin: 40px auto 42px;
-  padding: 55px 69px 50px 69px;
-  border: 1px solid rgba(0, 0, 0, 0.12);
-  font-size: 12px;
-  box-sizing: border-box;
-}
-.box_tf .tf_g {
-  position: relative;
-  z-index: 1;
-  width: 100%;
-  min-height: 45px;
-  padding: 10px 0 8px;
-  border: 0;
-  font-size: 18px;
-  line-height: 25px;
-  color: #191919;
-  background-color: transparent;
-  letter-spacing: 0;
-  box-sizing: border-box;
-  outline: 0 none;
-  caret-color: #191919;
-  opacity: 1;
-}
-.box_tf {
-  position: relative;
-  border: solid #ccc;
-  border-width: 0 0 2px;
-  font-family: -apple-system, Roboto, NotoSans, AppleSDGothicNeo, Malgun Gothic,
-    맑은 고딕, 돋움, dotum, sans-serif;
-}
-.set_login {
-  margin-top: 25px;
-}
-.confirm_btn {
-  padding-top: 40px;
-  text-align: center;
-}
-.screen_out {
-  /* position: absolute; */
-  width: 0;
-  height: 0;
-  color: white;
-}
-.line_or {
-  position: relative;
-  display: block;
-  width: 100%;
-  padding: 15px 0;
-  font-size: 12;
-  line-height: 2;
-}
-dl,
-li,
-ol,
-ul {
-  list-style: none;
-  text-decoration: none;
-}
-.link_user {
-  text-align: right;
-}
-</style>
+<style scoped></style>
