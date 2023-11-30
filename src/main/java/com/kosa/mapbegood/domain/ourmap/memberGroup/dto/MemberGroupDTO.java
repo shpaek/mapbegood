23fdb450bf495df3b42b26113ba8e84e8c3eab63
tofubeat <0717jis@daum.net>
2023-11-30 17:45:46 +0900

    fix: group func

--- src/main/java/com/kosa/mapbegood/domain/ourmap/memberGroup/dto/MemberGroupDTO.java
+++ src/main/java/com/kosa/mapbegood/domain/ourmap/memberGroup/dto/MemberGroupDTO.java
@@ -1,6 +1,14 @@
 package com.kosa.mapbegood.domain.ourmap.memberGroup.dto;
 
-import lombok.*;
+import com.kosa.mapbegood.domain.member.dto.MemberDTO;
+import com.kosa.mapbegood.domain.ourmap.groups.dto.GroupsDTO;
+
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+import lombok.ToString;
 
 @Setter @Getter @NoArgsConstructor @AllArgsConstructor @Builder @ToString
 public class MemberGroupDTO {
