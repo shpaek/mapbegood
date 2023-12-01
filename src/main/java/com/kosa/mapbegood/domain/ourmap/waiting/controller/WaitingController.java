5d5e1833e8d48e8408cfbfc2d91d8cd0ca07be09
tofubeat <0717jis@daum.net>
2023-12-01 14:24:23 +0900

    [Feat] Waiting 기능 구현
    - 수락대기 추가
    - 수락대기 삭제
    - 수락대기자명단 조회

--- src/main/java/com/kosa/mapbegood/domain/ourmap/waiting/controller/WaitingController.java
+++ src/main/java/com/kosa/mapbegood/domain/ourmap/waiting/controller/WaitingController.java
@@ -1,5 +1,54 @@
 package com.kosa.mapbegood.domain.ourmap.waiting.controller;
 
-public class WaitingController {
+import java.util.List;
 
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.DeleteMapping;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import com.kosa.mapbegood.domain.member.dto.MemberDTO;
+import com.kosa.mapbegood.domain.ourmap.waiting.dto.WaitingDTO;
+import com.kosa.mapbegood.domain.ourmap.waiting.service.WaitingService;
+import com.kosa.mapbegood.exception.AddException;
+import com.kosa.mapbegood.exception.FindException;
+import com.kosa.mapbegood.exception.RemoveException;
+
+@RestController
+@RequestMapping("/waiting")
+public class WaitingController {
+	@Autowired
+	private WaitingService ws;
+	
+	@GetMapping(value="", produces="application/json;charset=UTF-8")
+	public List<MemberDTO> findAllWaitingsByGroupId(@PathVariable Long groupId) throws FindException{
+		return ws.findAllWaitingsByGroupId(groupId);
+	}
+	
+	
+	@PostMapping(value="", produces="application/json;charset=UTF-8")
+	public ResponseEntity<?> createWaiting(@RequestBody WaitingDTO waitingDto) throws AddException{
+		try {
+			ws.createWaiting(waitingDto);
+			return new ResponseEntity<>(HttpStatus.OK);
+		}catch(AddException e) {
+			return new ResponseEntity<>(HttpStatus.BAD_REQUEST);
+		}
+	}
+	
+	@DeleteMapping(value="", produces="application/json;charset=UTF-8")
+	public ResponseEntity<?> deleteWaiting(@RequestBody WaitingDTO waitingDto) throws RemoveException{
+		try {
+			ws.deleteWaiting(waitingDto);
+			return new ResponseEntity<>(HttpStatus.OK);
+		}catch(RemoveException e) {
+			return new ResponseEntity<>(HttpStatus.BAD_REQUEST);
+		}
+	}
 }
--- src/main/java/com/kosa/mapbegood/domain/ourmap/waiting/service/WaitingService.java
+++ src/main/java/com/kosa/mapbegood/domain/ourmap/waiting/service/WaitingService.java
@@ -1,20 +1,28 @@
 package com.kosa.mapbegood.domain.ourmap.waiting.service;
 
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Optional;
+
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
+import com.kosa.mapbegood.domain.member.dto.MemberDTO;
 import com.kosa.mapbegood.domain.member.entity.Member;
-import com.kosa.mapbegood.domain.ourmap.groups.entity.Groups;
+import com.kosa.mapbegood.domain.member.repository.MemberRepository;
 import com.kosa.mapbegood.domain.ourmap.waiting.dto.WaitingDTO;
 import com.kosa.mapbegood.domain.ourmap.waiting.entity.Waiting;
 import com.kosa.mapbegood.domain.ourmap.waiting.repository.WaitingRepository;
 import com.kosa.mapbegood.exception.AddException;
+import com.kosa.mapbegood.exception.FindException;
 import com.kosa.mapbegood.exception.RemoveException;
 
 @Service
 public class WaitingService {
 	@Autowired
 	private WaitingRepository wr;
+	@Autowired
+	private MemberRepository mr;
 	
 	/**
 	 * WaitingDTO를 Waiting(엔터티)로 변환한다
@@ -23,7 +31,7 @@ public class WaitingService {
 	 */
 	public Waiting waitingDtoToEntity(WaitingDTO waitingDto) {
 		Waiting waitingEntity = new Waiting();
-		waitingEntity.setId(waitingDto.getId());
+//		waitingEntity.setId(waitingDto.getId());
 		waitingEntity.setGroupId(waitingDto.getGroupId());
 		waitingEntity.setMemberEmail(waitingDto.getMemberEmail());
 		
@@ -43,6 +51,35 @@ public class WaitingService {
 	}
 	
 	/**
+	 * 특정 그룹의 수락대기목록(명단)을 조회한다
+	 * @param groupId
+	 * @return 특정 그룹의 수락대기중인 사용자의 닉네임 
+	 * @throws FindException
+	 */
+	public List<MemberDTO> findAllWaitingsByGroupId(Long groupId) throws FindException{
+		List<MemberDTO> resultMemberDtoList = new ArrayList();
+		try {
+			List<Waiting> waitingList = wr.findByGroupId(groupId);
+			for(int i=0;i<waitingList.size();i++) {
+				Waiting waiting = waitingList.get(i); //수락대기 1개로 닉네임 찾기
+				Optional<Member> optMember = mr.findById(waiting.getMemberEmail());
+				if(optMember.isPresent()) {
+					Member member = optMember.get();
+					MemberDTO memberDto = new MemberDTO();
+					memberDto.setNickname(member.getNickname());
+					resultMemberDtoList.add(memberDto);
+				}
+			}			
+			return resultMemberDtoList;
+		}catch(Exception e) {
+			new FindException(e.getMessage());
+			return null;
+		}
+	}
+	
+	
+	
+	/**
 	 * 사용자에게 그룹초대 메시지를 보냈을 때 수락대기테이블에 사용자를 추가한다 
 	 * @param waitingDto
 	 * @throws AddException
@@ -52,12 +89,36 @@ public class WaitingService {
 		wr.save(entity);
 	}
 	
+	
+	/**
+	 * WaitingDTO로 Waiting이 있는지 조회하고 id값을 받아온다
+	 * @param waitingDto
+	 * @return Waiting(id값)
+	 * @throws FindException
+	 */
+	public Waiting findIdByGroupIdAndMemberEmail(WaitingDTO waitingDto) throws FindException{
+		Waiting waiting = new Waiting();
+		waiting = waitingDtoToEntity(waitingDto);
+		try {			
+			waiting = wr.findByGroupIdAndMemberEmail(waiting.getGroupId(), waiting.getMemberEmail()); //waiting id받아옴
+			return waiting;
+		}catch(Exception e) {
+			new FindException(e.getMessage());
+			return null;
+		}
+	}
+	
 	/**
 	 * 사용자가 그룹초대를 수락 또는 거절하면 수락대기 테이블에서 사용자를 제거한다
 	 * @param waitingId
 	 * @throws RemoveException
 	 */
-	public void deleteWaiting(Long waitingId) throws RemoveException{
-		wr.deleteById(waitingId);
+	public void deleteWaiting(WaitingDTO waitingDto) throws RemoveException{
+		try {
+			Waiting waiting = findIdByGroupIdAndMemberEmail(waitingDto);
+			wr.deleteById(waiting.getId());
+		} catch (Exception e) {
+			new RemoveException("수락대기 삭제 실패");
+		}
 	}
 }
