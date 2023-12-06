baa11734927f5d6f223ae313a58608b8f918dc17
tofubeat <0717jis@daum.net>
2023-12-06 15:11:32 +0900

    [Feat] front-group 그룹 전체 조회
    그룹 추가(수정)
    그룹 전체 조회

--- src/pages/GroupCreate.vue
+++ src/pages/GroupCreate.vue
@@ -8,7 +8,9 @@
                 <br>
                 <img class="img" :src="image" alt="그룹이미지">
                 <br>
-                <input type="file" name="img" id="i" @change="imageChangeHandler"/>
+                <input type="file" name="image" id="i" required
+                @change="imageChangeHandler"/>
+                <div v-if="fileErrorMsg">{{ fileErrorMsg }}</div>
             </div>
             <hr>
             <label for="n">그룹명</label>&nbsp;
@@ -33,65 +35,98 @@
 import axios from 'axios';
 export default {
     name: "GroupCreate",
-    data(){
-        return{    
+    data() {
+        return {
             image: '../../../public/images/defaultGroupProfile.jpg',
             name: '',
             // memberlist: [],  
             // member: '',
+            fileErrorMsg: null,
         }
     },
-    methods:{
-        groupcreateFormSubmitHandler(e){//그룹 생성 버튼 클릭 시
+    methods: {
+        groupcreateFormSubmitHandler(e) {//그룹 생성 버튼 클릭 시
             //axios로 백 url요청
-            const url = `${this.backURL}/group`
-            const fd = new FormData(e.target)
-            const data = fd
+            const url = "http://localhost:8080/mapbegood/group" //`${this.backURL}/group`
 
-            axios.post(url,data, {contentType: false, //파일첨부용 프로퍼티
-                                  processData: false})
-                 .then(response => {
-                    alert("결과:"+response.data.msg)
-                 })
-                 .catch(error => {
+            const fd = new FormData(e.target)
+
+            // const accessToken = "Bearer "+localStorage.getItem("mapbegoodToken")
+            const accessToken = localStorage.getItem("mapbegoodToken")
+            axios.defaults.headers.common["Authorization"] = accessToken;
+            // const data = fd
+
+            axios.post(url, fd, { contentType: false, processData: false, withCredentials: true })
+                .then(response => {
+                    alert("결과:" + response.data.msg)
+                })
+                .catch(error => {
                     alert(error.message)
-                 })
+                })
         },
-        imageChangeHandler(e){
+        imageChangeHandler(e) {
             const url = URL.createObjectURL(e.target.files[0]) //<input type="file">선택된 파일자원
             this.image = url //$('form.signup img.profile').attr('src', url)
+
+            const fileInput = e.target;
+            const file = fileInput.files[0];
+
+            // 이미지 파일만 허용
+            const allowedTypes = ['image/jpeg', 'image/jpg', 'image/png'];
+            if (!allowedTypes.includes(file.type)) {
+                this.fileErrorMsg = '이미지 파일만 허용됩니다.';
+                fileInput.value = ''; // 파일 선택 초기화
+                return;
+            }
+
+            // 파일 크기 제한 (2MB)
+            const maxSize = 2 * 1024 * 1024; // 2MB
+            if (file.size > maxSize) {
+                this.fileError = '파일 크기는 2MB를 초과할 수 없습니다.';
+                fileInput.value = ''; // 파일 선택 초기화
+                return;
+            }
+
+            // 유효성 검사를 통과한 경우
+            this.fileErrorMsg = null;
         },
-        b2ClickHandler(){ //생성 취소 버튼 클릭 시
+        b2ClickHandler() { //생성 취소 버튼 클릭 시
             alert("그룹 생성을 취소합니다")
-            location.href="/group"
+            location.href = "/group"
         },
-       
-       
+
+
     }
 }
 </script>
 <style scoped>
-div.fill{
-    margin-top:100px;
-    margin-bottom:100px;
-    margin-left:100px;
+div.fill {
+    margin-top: 100px;
+    margin-bottom: 100px;
+    margin-left: 100px;
     border: 1px solid lightgrey;
-    padding:25px;
+    padding: 25px;
     width: 500px;
 }
-div.fill>label{
-    font-size:15px;
+
+div.fill>label {
+    font-size: 15px;
 }
-div.fill>div.img>label{
-    font-size:15px;
+
+div.fill>div.img>label {
+    font-size: 15px;
 }
-div.fill>div.img>img.img-size{
-    max-width: 120px; /* 화면에서 보여줄 사이즈로 조정 */
-    max-height: 120px;
-    min-height: 120px;
-    min-width: 120px;
+
+div.fill>div.img>img.img {
+    /* 화면에서 보여줄 사이즈로 조정 */
+    max-width: 200px;
+    max-height: 200px;
+    min-height: 200px;
+    min-width: 200px;
+    
 }
-div.button-container{
+
+div.button-container {
     /* display: flex;
     justify-content: center; */
     margin-left: 235px;
