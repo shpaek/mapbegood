15964e42876f8b07eb25555642ea18af62a02a20
tofubeat <0717jis@daum.net>
2023-12-07 11:26:43 +0900

    [Fix] front-group 그룹 상세보기 수정
    작업중

---
+++ src/pages/GroupNameChange.vue
@@ -0,0 +1,19 @@
+<template lang="">
+    <div>
+        
+    </div>
+</template>
+<script>
+import axios from 'axios';
+export default {
+    name: 'GroupNameChange',
+    date(){
+        return{
+
+        }
+    }
+}
+</script>
+<style scoped>
+    
+</style>
