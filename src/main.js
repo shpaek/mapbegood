13fe370b4d70377b01695047bf084b17a4cac9b6
jt <kks9612161@gmail.com>
2023-12-07 17:47:34 +0900

    init

--- src/main.js
+++
@@ -1,8 +0,0 @@
-import { createApp } from "vue";
-import App from "./App.vue";
-import router from "./router";
-
-const app = createApp(App);
-app.use(router);
-app.config.globalProperties.backURL = "http://localhost:8080";
-app.mount("#app");
