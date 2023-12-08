3148893cbd82bcf5eeced3e60b1510eecef38f4a
tofubeat <0717jis@daum.net>
2023-12-08 09:29:49 +0900

    merge front-group

--- src/event-bus.js
+++
@@ -1,9 +0,0 @@
-// EventBus.js
-import { ref } from 'vue';
-
-export const EventBus = ref({
-  event: null,
-  emit(value) {
-    this.event = value;
-  },
-});
--- src/pages/ThememapCreate.vue
+++
@@ -1,1 +0,0 @@
-<!--ThememapCreate.vue-->
