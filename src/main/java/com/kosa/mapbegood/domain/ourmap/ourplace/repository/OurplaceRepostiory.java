7f5f0bc203505d1bbd7f65d7cee3445be06146be
karlos1315 <karlos1315@gmail.com>
2023-12-09 18:47:47 +0900

    feat:place

--- src/main/java/com/kosa/mapbegood/domain/ourmap/ourplace/repository/OurplaceRepostiory.java
+++ src/main/java/com/kosa/mapbegood/domain/ourmap/ourplace/repository/OurplaceRepostiory.java
@@ -2,8 +2,9 @@ package com.kosa.mapbegood.domain.ourmap.ourplace.repository;
 
 import org.springframework.data.jpa.repository.JpaRepository;
 
-import com.kosa.mapbegood.domain.mymap.myplace.entity.Myplace;
+import com.kosa.mapbegood.domain.ourmap.ourplace.entity.Ourplace;
+import com.kosa.mapbegood.domain.ourmap.ourplace.entity.Ourplace;
 
-public interface OurplaceRepostiory extends JpaRepository<Myplace, Long>  {
+public interface OurplaceRepostiory extends JpaRepository<Ourplace, Long>  {
 
 }
