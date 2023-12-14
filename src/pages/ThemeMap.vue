641f1f8c85a94662e0c116b9722d3b93c18b40a1
jt <kks9612161@gmail.com>
2023-12-14 15:23:20 +0900

    fix: style

--- src/pages/ThemeMap.vue
+++ src/pages/ThemeMap.vue
@@ -1,31 +1,46 @@
-<!--ThemeMap.vue-->
 <template>
-    <div class="content">
-      <h1>나의 테마지도리스트 입니다.</h1>
-      <ul>
-        <!-- favoriteList 내 각 항목에 대해 반복 -->
-        <!-- v-for을 사용해서 반복문 구현 -->
-        <!--in favaoriteList: 반복할 대상 데이터이며 , favoriteList 배열의 각 요소를 하나씩 순회함.-->
-        <!--key는 반복된 엘리먼트에 고유한 키를 제공하며, thememapDto.id를 사용-->
-        <li v-for="thememap in favoriteList" :key="thememap.themeMapDto.id">
-          <h3>{{ thememap.themeMapDto.name }}</h3>
-          <p>{{ thememap.themeMapDto.memo }}</p>
-          <p>{{ thememap.themeMapDto.id }}</p>
-            
-          <!-- "상세보기" 버튼 -->
-          <button @click="detailThememap(thememap.themeMapDto.id)">상세보기</button>
-          <!--리스트 수정 버튼-->
-          <button @click="editThememap(thememap.themeMapDto.id)">수정</button>
-          <!--리스트 삭제 버튼-->
-          <button @click="deleteThememap(thememap.themeMapDto.id)">삭제</button>
-          <!--리스트 복사 버튼-->
-          <button @click="copyThememap(thememap.themeMapDto.id)">복사</button>
+  <div class="content">
+    <h1 class="theme-list">나의 테마지도리스트</h1>
 
+    <!-- 각 테마맵에 대한 반복문 -->
+    <div v-for="thememap in favoriteList" :key="thememap.themeMapDto.id">
+      <ul class="elevated-list mb-4">
+        <li>
+          <h3>리스트이름:{{ thememap.themeMapDto.name }}</h3>
+          <h5>
+            <!-- 공개 여부에 따라 다른 아이콘 표시 -->
+            <span v-if="thememap.themeMapDto.show">
+              <svg xmlns="http://www.w3.org/2000/svg" width="30" height="30" fill="currentColor" class="bi bi-unlock-fill" viewBox="0 0 16 16">
+                <path d="M11 1a2 2 0 0 0-2 2v4a2 2 0 0 1 2 2v5a2 2 0 0 1-2 2H3a2 2 0 0 1-2-2V9a2 2 0 0 1 2-2h5V3a3 3 0 0 1 6 0v4a.5.5 0 0 1-1 0V3a2 2 0 0 0-2-2z"/>
+              </svg>
+            </span>
+            <span v-else>
+              <svg xmlns="http://www.w3.org/2000/svg" width="30" height="30" fill="currentColor" class="bi bi-lock-fill" viewBox="0 0 16 16">
+                <path d="M8 1a2 2 0 0 1 2 2v4H6V3a2 2 0 0 1 2-2zm3 6V3a3 3 0 0 0-6 0v4a2 2 0 0 0-2 2v5a2 2 0 0 0 2 2h6a2 2 0 0 0 2-2V9a2 2 0 0 0-2-2z"/>
+              </svg>
+            </span>
+          </h5>
+          <div class="text-right">
+            <!-- "상세보기" 버튼 -->
+            <button @click="detailThememap(thememap.themeMapDto.id)" class="btn btn-outline-secondary">상세보기</button>
+            <!-- 리스트 수정 버튼 -->
+            <button @click="editThememap(thememap.themeMapDto.id)" class="btn btn-outline-secondary">수정</button>
+            <!-- 리스트 삭제 버튼 -->
+            <button @click="deleteThememap(thememap.themeMapDto.id)" class="btn btn-outline-danger">삭제</button>
+            <!-- 리스트 복사 버튼 -->
+            <button @click="copyThememap(thememap.themeMapDto.id)" class="btn btn-outline-secondary">복사</button>
+          </div>
         </li>
       </ul>
-  
+    </div>
       <!-- "테마맵 추가" 버튼 -->
-      <button @click="addNewThememap">리스트 생성</button>
+      <!-- <button @click="addNewThememap" class="sticker-btn btn btn-outline-secondary">리스트 생성</button> -->
+      <button @click="addNewThememap" class="sticker-btn btn btn-outline-secondary">
+  <svg xmlns="http://www.w3.org/2000/svg" width="23" height="23" fill="currentColor" class="bi bi-file-earmark-plus-fill" viewBox="0 0 16 16">
+    <path d="M9.293 0H4a2 2 0 0 0-2 2v12a2 2 0 0 0 2 2h8a2 2 0 0 0 2-2V4.707A1 1 0 0 0 13.707 4L10 .293A1 1 0 0 0 9.293 0zM9.5 3.5v-2l3 3h-2a1 1 0 0 1-1-1zM8.5 7v1.5H10a.5.5 0 0 1 0 1H8.5V11a.5.5 0 0 1-1 0V9.5H6a.5.5 0 0 1 0-1h1.5V7a.5.5 0 0 1 1 0z"/>
+  </svg>
+</button>
+
     </div>
   </template>
   
@@ -133,35 +148,72 @@
     font-size: 1.5rem;
     margin-bottom: 1rem;
   }
-  
+
+  h1.list-heading {
+    color: #004080;
+    font-size: 2.5rem;
+    font-weight: bold;
+    margin-bottom: 1.5rem;
+  }
+  ul.theme-list {
+  list-style-type: none;
+  padding: 0;
+  border: 2px solid #4e4e52; /* Border color for the ul element */
+  border-radius: 10px; /* Border radius for rounded corners */
+  margin: 20px 0; /* Adjust margin as needed */
+}
+  .sticker-btn {
+    background-color: #4e4e52; /* Sticker background color */
+    border: 1px solid #000000; /* Border for the sticker */
+    padding: 0.75rem 1.5rem; /* Adjust padding for the sticker */
+    font-size: 1rem;
+    border-radius: 8px; /* Border radius for rounded corners */
+    cursor: pointer;
+    transition: background-color 0.3s ease; /* Add a smooth transition effect */
+  }
+  ul.elevated-list {
+  list-style-type: none;
+  padding: 0;
+  border: 2px solid #4e4e52;
+  border-radius: 10px;
+  margin: 20px 0;
+  background-color: #f8f9fa; /* Background color for the ul element */
+  box-shadow: 0 4px 8px rgba(0, 0, 0, 0.1); /* Box shadow for elevation */
+}
+  .sticker-btn:hover {
+    background-color: #e2e6e8; /* Change background color on hover */
+  }
+
   button {
     margin-right: 10px;
     margin-bottom: 0;
   }
-  
+
   ul {
     list-style-type: none;
     padding: 0;
+    max-width: 600px;
+    margin: 0;
   }
-  
+
   li {
     border: 1px solid #ccc;
     padding: 1rem;
     margin-bottom: 0.5rem;
   }
-  
+
   h3 {
-    color: #007bff;
+    color: #030d18;
     font-size: 1.2rem;
     margin-bottom: 0.5rem;
   }
-  
+
   p {
     color: #666;
     font-size: 1rem;
     margin-bottom: 0;
   }
-  
+
   .sidebar {
     position: fixed;
     top: 50%;
@@ -172,8 +224,9 @@
     border: 1px solid #ccc;
     border-radius: 5px;
   }
-  
+
   .content {
     margin-left: 200px;
   }
-  </style>
+</style>
+  
--- src/pages/ThememapCreate.vue
+++ src/pages/ThememapCreate.vue
@@ -1,40 +1,48 @@
 <!-- ThememapCreate.vue -->
 <template>
-    <div class="bg-gray-50">
-      <div class="max-w-md mx-auto my-10 bg-white p-6 rounded-lg shadow">
-        <h1 class="text-2xl font-semibold mb-4">테마지도 생성하기</h1>
-  
+  <div class="bg-dark">
+    <div class="container my-5">
+      <div class="card text-black bg-light p-4 rounded">
+        <h1 class="text-primary mb-4">테마지도 생성하기</h1>
+
         <!-- 테마 이름 입력 -->
-        <label for="themeName" class="block text-sm font-medium text-gray-700">테마 이름</label>
-        <input v-model="themeName" id="themeName" name="themeName" type="text" class="mt-1 p-2 border border-gray-300 rounded-md focus:outline-none focus:ring focus:border-blue-300">
-  
+        <div class="mb-3">
+          <label for="themeName" class="form-label text-black">테마 이름</label>
+          <input v-model="themeName" id="themeName" name="themeName" type="text" class="form-control">
+        </div>
+
         <!-- 색상 선택 드롭다운 -->
-        <label for="colorSelector" class="block text-sm font-medium text-gray-700 mt-4">테마 색상 선택</label>
-        <select v-model="selectedColor" id="colorSelector" name="colorSelector" class="mt-1 p-2 border border-gray-300 rounded-md focus:outline-none focus:ring focus:border-blue-300">
-          <option v-for="color in colors" :key="color" :value="color">{{ color }}</option>
-        </select>
-  
+        <div class="mb-3">
+          <label for="colorSelector" class="form-label text-black">테마 색상 선택</label>
+          <select v-model="selectedColor" id="colorSelector" name="colorSelector" class="form-select">
+            <option v-for="color in colors" :key="color" :value="color">{{ color }}</option>
+          </select>
+        </div>
+
         <!-- 테마 메모 입력 -->
-        <label for="themeMemo" class="block text-sm font-medium text-gray-700 mt-4">테마 메모</label>
-        <textarea v-model="themeMemo" id="themeMemo" name="themeMemo" rows="4" class="mt-1 p-2 border border-gray-300 rounded-md focus:outline-none focus:ring focus:border-blue-300"></textarea>
-  
+        <div class="mb-3">
+          <label for="themeMemo" class="form-label text-black">테마 메모</label>
+          <textarea v-model="themeMemo" id="themeMemo" name="themeMemo" rows="4" class="form-control"></textarea>
+        </div>
+
         <!-- 테마 공개 여부 체크박스 -->
-        <label class="flex items-center mt-4">
-          <input v-model="isThemePublic" type="checkbox" class="form-checkbox text-blue-500">
-          <span class="ml-2 text-sm">테마 공개 여부</span>
-        </label>
-  
+        <div class="mb-3 form-check">
+          <input v-model="isThemePublic" type="checkbox" class="form-check-input" id="showCheckbox">
+          <label class="form-check-label text-black" for="showCheckbox">테마 공개 여부</label>
+        </div>
+
         <!-- 메인맵 여부 체크박스 -->
-        <label class="flex items-center mt-2">
-          <input v-model="isMainMap" type="checkbox" class="form-checkbox text-blue-500">
-          <span class="ml-2 text-sm">메인맵으로 설정</span>
-        </label>
-  
+        <div class="mb-3 form-check">
+          <input v-model="isMainMap" type="checkbox" class="form-check-input" id="mainmapCheckbox">
+          <label class="form-check-label text-black" for="mainmapCheckbox">메인맵으로 설정</label>
+        </div>
+
         <!-- 테마맵 생성 버튼 -->
-        <button @click="createThemeMap" class="mt-4 p-2 bg-blue-500 text-white rounded-md">테마맵 생성</button>
+        <button @click="createThemeMap" class="btn btn-dark">테마맵 생성</button>
       </div>
     </div>
-  </template>
+  </div>
+</template>
   
   <script>
   import axios from 'axios'
