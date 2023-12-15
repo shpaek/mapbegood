204b14f87ad391555e5169f85353ba37a582e12e
tofubeat <0717jis@daum.net>
2023-12-15 10:34:32 +0900

    Fix: groupleader조건처리

--- src/pages/GroupsView.vue
+++ src/pages/GroupsView.vue
@@ -1,24 +1,21 @@
 <template lang="">
   <h2>내 그룹</h2>
+  <div class="icon-container">
+    <span class="check-waiting">
+      <svg xmlns="http://www.w3.org/2000/svg" width="30" height="30" fill="currentColor" class="bi bi-envelope" viewBox="0 0 16 16">
+        <path d="M0 4a2 2 0 0 1 2-2h12a2 2 0 0 1 2 2v8a2 2 0 0 1-2 2H2a2 2 0 0 1-2-2zm2-1a1 1 0 0 0-1 1v.217l7 4.2 7-4.2V4a1 1 0 0 0-1-1zm13 2.383-4.708 2.825L15 11.105zm-.034 6.876-5.64-3.471L8 9.583l-1.326-.795-5.64 3.47A1 1 0 0 0 2 13h12a1 1 0 0 0 .966-.741M1 11.105l4.708-2.897L1 5.383z"/>
+      </svg>
+    </span>
+  </div>
   <div class="group-container">
-    <div
-      class="group"
-      v-for="group in groupList"
-      @click="groupClickHandler(group)"
-    >
+    <div class="add-group">
+      <span class="plus">+</span>
+    </div>
+    <div class="group" v-for="group in groupList"
+      @click="groupClickHandler(group)">
       <ul>
         <li>
-          <img
-            id="i"
-            alt="그룹이미지"
-            class="img-size"
-            :src="
-              'https://mapbegood-image.s3.ap-northeast-2.amazonaws.com/group-image/' +
-              group.id +
-              '_groupImage.jpg?' +
-              new Date().getTime()
-            "
-          />
+          <img id="i" alt="그룹이미지" class="img-size" :src="'https://mapbegood-image.s3.ap-northeast-2.amazonaws.com/group-image/' + group.id +'_groupImage.jpg?' +new Date().getTime()"/>
         </li>
         <li>
           <span class="group-info">{{ group.name }}</span>
@@ -32,25 +29,21 @@
     </div>
     <span class="empty-msg">{{ emptyMsg }}</span>
   </div>
-  <br />
-  <span class="add-group" @click="addgroupClickHandler">
-    <svg
-      xmlns="http://www.w3.org/2000/svg"
-      width="30"
-      height="30"
-      fill="currentColor"
-      class="bi bi-plus-circle-fill"
-      viewBox="0 0 16 16"
-    >
-      <path
-        d="M16 8A8 8 0 1 1 0 8a8 8 0 0 1 16 0zM8.5 4.5a.5.5 0 0 0-1 0v3h-3a.5.5 0 0 0 0 1h3v3a.5.5 0 0 0 1 0v-3h3a.5.5 0 0 0 0-1h-3v-3z"
-      />
-    </svg>
-  </span>
+  <span class="add-group" >
+      <svg xmlns="http://www.w3.org/2000/svg" width="30" height="30" fill="currentColor" class="bi bi-plus-circle-fill" viewBox="0 0 16 16"
+       @click="addgroupClickHandler">
+      <path d="M16 8A8 8 0 1 1 0 8a8 8 0 0 1 16 0zM8.5 4.5a.5.5 0 0 0-1 0v3h-3a.5.5 0 0 0 0 1h3v3a.5.5 0 0 0 1 0v-3h3a.5.5 0 0 0 0-1h-3v-3z" />
+      </svg>
+      <!-- <svg xmlns="http://www.w3.org/2000/svg" width="30" height="30" fill="currentColor" class="bi bi-plus-circle" viewBox="0 0 16 16"
+        >
+        <path d="M8 15A7 7 0 1 1 8 1a7 7 0 0 1 0 14m0 1A8 8 0 1 0 8 0a8 8 0 0 0 0 16"/>
+        <path d="M8 4a.5.5 0 0 1 .5.5v3h3a.5.5 0 0 1 0 1h-3v3a.5.5 0 0 1-1 0v-3h-3a.5.5 0 0 1 0-1h3v-3A.5.5 0 0 1 8 4"/>
+      </svg> -->
+    </span>
 </template>
 <script>
 import axios from "axios";
-
+import Waiting from '';
 export default {
   name: "GroupsView",
   data() {
@@ -143,16 +136,41 @@ div.group > ul > li > img.img-size {
   min-height: 150px;
   display: inline;
 }
-
 span.add-group {
   display: flex;
-  margin-left: 30px;
-  margin-bottom: 50px;
+  margin-left: 100px;
+  margin-top: 30px;
+  width: 30px;
+}
+span.check-waiting {
+  display: flex;
+  /* margin-left: 80%; 주석 처리 */
+  margin-left: 85%; /* 적절한 간격을 위해 수정 */
+  margin-top: 30px;
+  width: 30px;
+  /* margin-right: 20%; 주석 처리 */
+  margin-right: 100px; /* 적절한 간격을 위해 수정 */
+}
+div.icon-container{
+  display: flex;
 }
 span.empty-msg {
   margin-top: 75px;
   margin-bottom: 50px;
   font-size: 14px;
   color: darkgray;
+}
+div.add-group{
+  display: flex;
+}
+div.add-group>span.plus{
+  margin-left: auto;
+  margin-right: auto;
+  margin-top: auto;
+  margin-bottom: auto;
+  width:150px;
+  height:150px;
+  font-size: 100px;
+  border: 3px solid black;
 }
 </style>
