992609005b2a60799452a865d48c3500518beada
dozy <78740368+yeori316@users.noreply.github.com>
2023-12-18 23:52:08 +0900

    fix: recommend

--- src/main/java/com/kosa/mapbegood/domain/mymap/favorite/controller/ReCommendThemeMapController.java
+++ src/main/java/com/kosa/mapbegood/domain/mymap/favorite/controller/ReCommendThemeMapController.java
@@ -9,7 +9,6 @@ import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RestController;
 
 import java.util.List;
-import java.util.Objects;
 import java.util.Optional;
 
 @Slf4j
