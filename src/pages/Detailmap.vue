4d4ac56f5c49a61b03a17c7cd65144a3c1516de4
karlos1315 <karlos1315@gmail.com>
2023-12-18 16:45:28 +0900

    feat:place

--- src/pages/Detailmap.vue
+++ src/pages/Detailmap.vue
@@ -5,6 +5,8 @@
 </template>
 
 <script>
+import axios from "axios";
+
 export default {
   name: "DetailMap",
   props: {
@@ -15,15 +17,11 @@ export default {
   },
   data() {
     return {
-      mapContainer: null,
-      markers: [],
       map: null,
-      ps: null,
-      infowindow: null,
+      markers: [],
     };
   },
   mounted() {
-    this.mapContainer = document.getElementById("map");
     this.loadScript();
   },
 
@@ -47,21 +45,18 @@ export default {
           throw new Error("Kakao Maps API가 로드되지 않았습니다.");
         }
 
-        this.ps = window.kakao.maps.services.Places;
-        this.infowindow = new window.kakao.maps.InfoWindow({ zIndex: 1 });
-
         const userLocation = await this.getCurrentLocation();
         this.setInitialMap(userLocation.latitude, userLocation.longitude);
-        this.getMyLocation();
+        // this.getMyLocation();
 
-        // 여러 장소의 마커를 표시
-        this.displayPlacesOnMap(places);
+        // Display markers on the map
+        this.displayPlacesOnMap();
       } catch (error) {
         console.error("지도 초기화 오류:", error);
       }
     },
-  
-      getCurrentLocation() {
+
+    getCurrentLocation() {
       return new Promise((resolve, reject) => {
         if (navigator.geolocation) {
           navigator.geolocation.getCurrentPosition(
@@ -82,25 +77,15 @@ export default {
     },
 
     setInitialMap(latitude, longitude) {
-      if (!window.kakao.maps.services || !window.kakao.maps.services.Places) {
-        window.kakao.maps.load(() => {
-          // Initialize Places service
-          window.kakao.maps.services.Places = new window.kakao.maps.services.Places();
-          this.ps = window.kakao.maps.services.Places;
-          this.infowindow = new window.kakao.maps.InfoWindow({ zIndex: 1 });
-        });
-      } else {
-        // Places service is already defined
-        this.ps = new window.kakao.maps.services.Places();
-        this.infowindow = new window.kakao.maps.InfoWindow({ zIndex: 1 });
-      }
-
       const mapOption = {
         center: new window.kakao.maps.LatLng(latitude, longitude),
         level: 3,
       };
 
-      this.map = new window.kakao.maps.Map(this.mapContainer, mapOption);
+      this.map = new window.kakao.maps.Map(
+        document.getElementById("map"),
+        mapOption
+      );
 
       // Add mapTypeControl and zoomControl
       const mapTypeControl = new window.kakao.maps.MapTypeControl();
@@ -108,57 +93,41 @@ export default {
       this.map.addControl(zoomControl, window.kakao.maps.ControlPosition.RIGHT);
     },
 
-    getMyLocation() {
-      this.getCurrentLocation()
-        .then((position) => {
-          const myLocation = new window.kakao.maps.LatLng(
-            position.latitude,
-            position.longitude
-          );
-          this.map.setCenter(myLocation);
-        })
-        .catch((error) => {
-          console.error("Error getting my location:", error);
-          alert("현재 위치를 가져올 수 없습니다.");
+    // getMyLocation() {
+    //   this.getCurrentLocation()
+    //     .then((position) => {
+    //       const myLocation = new window.kakao.maps.LatLng(
+    //         position.latitude,
+    //         position.longitude
+    //       );
+    //       this.map.setCenter(myLocation);
+    //     })
+    //     .catch((error) => {
+    //       console.error("Error getting my location:", error);
+    //       alert("현재 위치를 가져올 수 없습니다.");
+    //     });
+    // },
+
+    displayPlacesOnMap() {
+  const bounds = new window.kakao.maps.LatLngBounds();
+
+  // Remove existing markers
+  this.removeMarkers();
+
+  // Check if myplaces is an array and not empty
+  if (Array.isArray(this.mymapdetail.myplaces) && this.mymapdetail.myplaces.length > 0) {
+    // Display markers on the map
+    this.mymapdetail.myplaces.forEach((place) => {
+      if (place.placeId && place.placeId.x && place.placeId.y) {
+        const placePosition = new window.kakao.maps.LatLng(
+          place.placeId.y,
+          place.placeId.x
+        );
+
+        const marker = new window.kakao.maps.Marker({
+          position: placePosition,
         });
-    },
 
-    centerMap(placePosition) {
-      console.log("Centering map at:", placePosition);
-      this.map.setCenter(placePosition);
-    },
-
-    setZoomLevel(level) {
-      if (this.map) {
-        this.map.setLevel(level);
-      }
-    },
-    // ... existing code ...
-
-    removeMarkers() {
-    if (this.markers && this.markers.length > 0) {
-      for (let i = 0; i < this.markers.length; i++) {
-        this.markers[i].setMap(null);
-      }
-      this.markers = [];
-    }
-  },
-  setMapBounds(bounds) {
-    // Map 객체의 중심과 확대 레벨을 설정합니다.
-    this.map.setBounds(bounds);
-  },
-
- // 여러 장소의 마커를 표시
- displayPlacesOnMap(places) {
-      const bounds = new window.kakao.maps.LatLngBounds();
-
-      // 기존 마커 및 목록 제거
-      this.removeMarkers();
-
-      // 마커 생성 및 지도에 표시
-      places.forEach((place, index) => {
-        const placePosition = new window.kakao.maps.LatLng(place.y, place.x);
-        const marker = this.addMarker(placePosition, place, index);
         marker.setMap(this.map);
 
         window.kakao.maps.event.addListener(marker, "click", () => {
@@ -166,77 +135,35 @@ export default {
         });
 
         bounds.extend(placePosition);
-      });
+        this.markers.push(marker);
+      }
+    });
+  } else {
+    console.warn("No places to display on the map.");
+  }
 
-      // 지도의 중심 및 확대 레벨 설정
-      this.setMapBounds(bounds);
-    },
+  // Set the map bounds even if there are no markers
+  this.map.setBounds(bounds);
+},
 
-    addMarker(position, place, index) {
-      const imageSize = new window.kakao.maps.Size(36, 37);
-
-      if (!this.markers || !Array.isArray(this.markers)) {
+    removeMarkers() {
+      if (this.markers && this.markers.length > 0) {
+        this.markers.forEach((marker) => {
+          marker.setMap(null);
+        });
         this.markers = [];
       }
-
-      if (place) {
-        const placeIndex = (index % 15) + 1;
-        // const placeIndex = index % 15;
-        const markerImage = new window.kakao.maps.MarkerImage(
-          `https://t1.daumcdn.net/localimg/localimages/07/mapapidoc/marker_number_blue.png#${placeIndex}`,
-          imageSize,
-          {
-            spriteSize: new window.kakao.maps.Size(36, 691),
-            spriteOrigin: new window.kakao.maps.Point(0, placeIndex * 46 + 10),
-            offset: new window.kakao.maps.Point(13, 37),
-          }
-        );
-
-        const marker = new window.kakao.maps.Marker({
-          position: position,
-          image: markerImage,
-        });
-
-        this.markers.push(marker);
-
-        // Custom code to handle marker click event
-        window.kakao.maps.event.addListener(marker, "click", () => {
-          // You can customize the behavior when a marker is clicked
-          console.log("Marker clicked:", place);
-          // For example, center the map on the clicked marker
-          this.centerMap(position);
-        });
-
-        return marker;
-      }
-    },
-
-    addBookmark(place) {
-      const url = `${this.backURL}/place/${place.id}`;
-      axios
-        .post(url, {
-          placeId: place.id,
-          name: place.place_name /* 기타 정보 */,
-        })
-        .then((response) => {
-          console.log("Bookmark added successfully:", response.data);
-        })
-        .catch((error) => {
-          console.error("Error adding bookmark:", error);
-        });
     },
   },
-  
 };
 </script>
-  
-  <style scoped>
-  #map {
-    width: 100%;
-    height: 100vh;
-    position: relative;
-    overflow: hidden;
-    z-index: 1;
-  }
-  </style>
-  
+
+<style scoped>
+#map {
+  width: 100%;
+  height: 100vh;
+  position: relative;
+  overflow: hidden;
+  z-index: 1;
+}
+</style>
--- src/pages/ThemeMapDetail.vue
+++ src/pages/ThemeMapDetail.vue
@@ -1,46 +1,29 @@
 <template>
   <!-- 전체화면에 맵 표시 -->
-  <DetailMap :mymapdetail="mymapdetail" :places="mymapdetail.myplaces" />
+  <DetailMap ref="detailMap" :mymapdetail="mymapdetail" :places="mymapdetail.myplaces" />
 
   <div class="theme-map-details">
-    <h2 class="mb-1" text-warning>
-      <svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor" class="bi bi-caret-right-fill" viewBox="0 0 16 16">
-        <path d="m12.14 8.753-5.482 4.796c-.646.566-1.658.106-1.658-.753V3.204a1 1 0 0 1 1.659-.753l5.48 4.796a1 1 0 0 1 0 1.506z"/>
-      </svg>
-      {{ mymapdetail.themeMapDto.name }}
-    </h2>
-    
-    <div class="card">
-      <div class="card-body">
-        <h5 class="card-title" text-warning>
-          <svg xmlns="http://www.w3.org/2000/svg" width="14" height="14" fill="currentColor" class="bi bi-chat-left-text" viewBox="0 0 16 16">
-            <path d="M14 1a1 1 0 0 1 1 1v8a1 1 0 0 1-1 1H4.414A2 2 0 0 0 3 11.586l-2 2V2a1 1 0 0 1 1-1h12zM2 0a2 2 0 0 0-2 2v12.793a.5.5 0 0 0 .854.353l2.853-2.853A1 1 0 0 1 4.414 12H14a2 2 0 0 0 2-2V2a2 2 0 0 0-2-2H2z"/>
-            <path d="M3 3.5a.5.5 0 0 1 .5-.5h9a.5.5 0 0 1 0 1h-9a.5.5 0 0 1-.5-.5zM3 6a.5.5 0 0 1 .5-.5h9a.5.5 0 0 1 0 1h-9A.5.5 0 0 1 3 6zm0 2.5a.5.5 0 0 1 .5-.5h5a.5.5 0 0 1 0 1h-5a.5.5 0 0 1-.5-.5z"/>
-          </svg>
-          내용:
-        </h5>
-        <p class="card-text">{{ mymapdetail.themeMapDto.memo }}</p>
-      </div>
-    </div>
-  <!--<p>아이디(가릴예정): {{ mymapdetail.themeMapDto.id }}</p>-->
+    <h1>{{ mymapdetail.themeMapDto.name }}</h1>
+    <p>내용(없어도될듯): {{ mymapdetail.themeMapDto.memo }}</p>
+    <p>아이디(가릴예정): {{ mymapdetail.themeMapDto.id }}</p>
 
     <div class="myplace-list-container">
-      <ul class="myplace-list">
-        <li
-          v-for="myplace in mymapdetail.myplaces"
-          :key="myplace.id"
-          class="myplace-item"
-        >
-          <div class="myplace-info">
+        <ul class="myplace-list">
+          <li
+            v-for="myplace in mymapdetail.myplaces"
+            :key="myplace.id"
+            class="myplace-item"
+          >
+            <div class="myplace-info">
             <h5>{{ myplace.placeId.placeName }}</h5>
             <p>방문 일자: {{ myplace.visitedAt }}</p>
             <p>주소: {{ myplace.placeId.address }}</p>
             <!-- 추가적인 Myplace 정보 표시 -->
           </div>
-          <button
-            class="add-bookmark-btn"
-            @click.stop="addBookmark(myplace.placeId)"
-          >
+            <button
+              class="add-bookmark-btn"
+              @click.stop="addBookmark(myplace.placeId)"
+            >
             <img src="/public/images/bookmark.png" class="bookmark-icon" />
             북마크
           </button>
@@ -75,42 +58,30 @@ export default {
     const themeMapId = this.$route.params.id;
     this.loadThemeMapDetail(themeMapId);
   },
-
   methods: {
-
-    
     async loadThemeMapDetail(themeMapId) {
-  const url = `${this.backURL}/mymap/${themeMapId}`;
-
-  try {
-    const response = await axios.get(url, {
-      withCredentials: true,
-      headers: {
-        Authorization: `Bearer ${localStorage.getItem("mapbegoodToken")}`,
-      },
-    });
-
-    // 받은 데이터를 처리하고 mymapdetail 객체를 채웁니다.
-    this.mymapdetail = {
-      themeMapDto: response.data,
-      myplaces: response.data.myplaces, // 추가: myplaces 데이터를 할당
-    };
-
-    // 추가: Myplaces 불러오기
-    this.findAllMyPlace(themeMapId);
-  } catch (error) {
-    console.error(error);
-    alert(
-      error.response.data.message ||
-        "테마 맵 세부 정보를 불러오지 못했습니다."
-    );
-  }
-},
-
+      const url = `${this.backURL}/mymap/${themeMapId}`;
+      try {
+        const response = await axios.get(url, {
+          withCredentials: true,
+          headers: {
+            Authorization: `Bearer ${localStorage.getItem("mapbegoodToken")}`,
+          },
+        });
+        this.mymapdetail = {
+          themeMapDto: response.data,
+        };
+        this.findAllMyPlace(themeMapId);
+      } catch (error) {
+        console.error(error);
+        alert(
+          error.response.data.message ||
+            "테마 맵 세부 정보를 불러오지 못했습니다."
+        );
+      }
+    },
     async findAllMyPlace(themeMapId) {
-      // Myplaces 불러오기
       const myPlacesUrl = `${this.backURL}/myplace/${themeMapId}`;
-
       try {
         const response = await axios.get(myPlacesUrl, {
           withCredentials: true,
@@ -118,9 +89,9 @@ export default {
             Authorization: `Bearer ${localStorage.getItem("mapbegoodToken")}`,
           },
         });
-
-        // 받은 Myplaces 데이터를 mymapdetail에 추가
         this.mymapdetail.myplaces = response.data;
+        // Call the method in the child component
+        this.$refs.detailMap.displayPlacesOnMap(response.data);
       } catch (error) {
         console.error(error);
         alert(error.response.data.message || "Myplaces를 불러오지 못했습니다.");
