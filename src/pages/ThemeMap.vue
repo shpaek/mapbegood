df1ab7afcba1f109fdb91944baa4529cedbf5ef7
karlos1315 <karlos1315@gmail.com>
2023-12-19 17:31:18 +0900

    feat:addbookmark design

--- src/pages/ThemeMap.vue
+++ src/pages/ThemeMap.vue
@@ -27,7 +27,7 @@
         <ul class="elevated-list mb-4">
           <li>
             <h3 @click="detailThememap(thememap.themeMapDto.id)">
-              리스트이름:{{ thememap.themeMapDto.name }}
+              {{ thememap.themeMapDto.name }}
             </h3>
             <h5>
               <!-- 공개 여부에 따라 다른 아이콘 표시 -->
