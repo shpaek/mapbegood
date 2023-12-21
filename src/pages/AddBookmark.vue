2a02f6a1f177fbad0496a1a7b410a9406e075ebc
dozy <78740368+yeori316@users.noreply.github.com>
2023-12-22 01:18:55 +0900

    fix: map

--- src/pages/AddBookmark.vue
+++ src/pages/AddBookmark.vue
@@ -184,8 +184,7 @@ export default {
     },
 
     loadMymapList() {
-      const url =
-        "http://ec2-15-164-213-115.ap-northeast-2.compute.amazonaws.com:8080/mymap/list";
+      const url = "https://api.mapbegood.site/mymap/list";
       const accessToken = "Bearer " + localStorage.getItem("mapbegoodToken");
       axios.defaults.headers.common["Authorization"] = accessToken;
 
